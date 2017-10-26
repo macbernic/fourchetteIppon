2fa6145910a0e3e8326a38b9ad4be5846bbaed07
mcoudert <mcoudert@ippon.fr>
2017-10-26 15:18:54 +0200

    ajout table restau dans home

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
-import { JhiEventManager } from 'ng-jhipster';
+import { JhiEventManager, JhiParseLinks, JhiAlertService } from 'ng-jhipster';
+import { RestaurantService } from '../entities/restaurant/restaurant.service';
+import { Restaurant } from '../entities/restaurant/restaurant.model';
 
-import { Account, LoginModalService, Principal } from '../shared';
+import { Account, LoginModalService, Principal, ITEMS_PER_PAGE } from '../shared';
+import {ResponseWrapper} from "../shared/model/response-wrapper.model";
 
 @Component({
     selector: 'jhi-home',
@@ -12,15 +15,28 @@ import { Account, LoginModalService, Principal } from '../shared';
     ]
 
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
     account: Account;
     modalRef: NgbModalRef;
+    restaurants: Restaurant[];
+    itemsPerPage: number;
+    links: any;
+    page: any;
 
     constructor(
         private principal: Principal,
         private loginModalService: LoginModalService,
-        private eventManager: JhiEventManager
+        private eventManager: JhiEventManager,
+        private restaurantService: RestaurantService,
+        private jhiAlertService: JhiAlertService
     ) {
+        this.restaurants = [];
+        this.itemsPerPage = ITEMS_PER_PAGE;
+        this.page = 0;
+        this.links = {
+            last: 0
+        };
+
     }
 
     ngOnInit() {
@@ -28,6 +44,7 @@ export class HomeComponent implements OnInit {
             this.account = account;
         });
         this.registerAuthenticationSuccess();
+        this.loadRestaurant();
     }
 
     registerAuthenticationSuccess() {
@@ -45,4 +62,32 @@ export class HomeComponent implements OnInit {
     login() {
         this.modalRef = this.loginModalService.open();
     }
+
+
+    reset() {
+        this.page = 0;
+        this.restaurants = [];
+        this.loadRestaurant();
+    }
+
+    loadRestaurant() {
+        this.restaurantService.query().subscribe(
+            (res: ResponseWrapper) => this.onSuccess(res.json, res.headers),
+            (res: ResponseWrapper) => this.onError(res.json)
+        );
+    }
+    private onSuccess(data, headers) {
+        for (let i = 0; i < data.length; i++) {
+            this.restaurants.push(data[i]);
+        }
+    }
+
+    private onError(error) {
+        this.jhiAlertService.error(error.message, null, null);
+    }
+
+    ngOnDestroy() {
+        //this.eventManager.destroy(this.eventSubscriber);
+    }
+
 }
